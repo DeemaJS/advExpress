770684b57defd4d1adb7e3cb4debe258b1fbc626
DeemaJS <deeema@outlook.com>
2016-08-01 15:25:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-var express = require('express');
+var app = require('express')();
+var serveStatic = require('serve-static');
+
+app.use(serveStatic(__dirname + '/public'));
+
+app.listen(4000);
+console.log('Server start on 4000');
